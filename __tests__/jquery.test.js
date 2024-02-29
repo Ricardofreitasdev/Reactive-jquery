dc98cd038700d0b0135e112d268ff2269d73b46e
Ricardo Freitas <freitasdev7@gmail.com>
2024-02-29 12:45:13 -0300

    adicionando favicon

--- __tests__/jquery.test.js
+++ __tests__/jquery.test.js
@@ -1,40 +1,50 @@
-import { $ } from '../jquery.js'
-import { test, describe } from 'node:test';
-import assert from 'node:assert';
-import { JSDOM } from 'jsdom'; 
+import { $ } from "../jquery.js";
+import { test, describe } from "node:test";
+import assert from "node:assert";
+import { JSDOM } from "jsdom";
 
+describe("jquery", () => {
+  test("AddClass", async () => {
+    const dom = new JSDOM(
+      '<!DOCTYPE html><div id="element1"></div><div id="element2"></div>'
+    );
+    const { document } = dom.window;
 
-describe('jquery', () => {
-    test('AddClass', async () => {
-        const dom = new JSDOM('<!DOCTYPE html><div id="element1"></div><div id="element2"></div>');
-        const { document } = dom.window;
-    
-        global.document = document;
-    
-        const $elements = $('#element1, #element2');
-    
-        $elements.addClass('test-class');
-    
-        const element1 = document.getElementById('element1');
-        const element2 = document.getElementById('element2');
-        
-        assert.ok(element1.classList.contains('test-class'), 'A classe não foi adicionada ao elemento 1');
-        assert.ok(element2.classList.contains('test-class'), 'A classe não foi adicionada ao elemento 2');
-    });
-    
-    test('Append', async () => {
-        const dom = new JSDOM('<!DOCTYPE html><div id="element"></div>');
-    
-        const { document } = dom.window;
-    
-        global.document = document;
-    
-        const $element = $('#element');
-    
-        $element.append('<span>Conteúdo adicionado</span>');
-    
-        const addedContent = document.querySelector('#element').innerHTML;
-        assert.strictEqual(addedContent, '<span>Conteúdo adicionado</span>', 'O conteúdo não foi adicionado corretamente ao elemento');
-    })
+    global.document = document;
+
+    const $elements = $("#element1, #element2");
+
+    $elements.addClass("test-class");
+
+    const element1 = document.getElementById("element1");
+    const element2 = document.getElementById("element2");
+
+    assert.ok(
+      element1.classList.contains("test-class"),
+      "A classe não foi adicionada ao elemento 1"
+    );
+    assert.ok(
+      element2.classList.contains("test-class"),
+      "A classe não foi adicionada ao elemento 2"
+    );
+  });
+
+  test("Append", async () => {
+    const dom = new JSDOM('<!DOCTYPE html><div id="element"></div>');
+
+    const { document } = dom.window;
+
+    global.document = document;
+
+    const $element = $("#element");
+
+    $element.append("<span>Conteúdo adicionado</span>");
+
+    const addedContent = document.querySelector("#element").innerHTML;
+    assert.strictEqual(
+      addedContent,
+      "<span>Conteúdo adicionado</span>",
+      "O conteúdo não foi adicionado corretamente ao elemento"
+    );
+  });
 });
-
--- jquery.js
+++ jquery.js
@@ -3,109 +3,119 @@
  * permitindo seleção de elementos, manipulação do DOM e eventos.
  */
 class JQuery {
-    /**
-     * Cria uma nova instância da classe JQuery.
-     * @param {string} selector - O seletor para selecionar elementos no DOM.
-     */
-    constructor(selector) {
-        this.elements = this.selectElements(selector);
+  /**
+   * Cria uma nova instância da classe JQuery.
+   * @param {string} selector - O seletor para selecionar elementos no DOM.
+   */
+  constructor(selector) {
+    this.elements = this.selectElements(selector);
+  }
+
+  /**
+   * Seleciona elementos no DOM com base no seletor fornecido.
+   * @param {string} selector - O seletor para selecionar elementos no DOM.
+   * @returns {NodeList} - Lista de elementos selecionados.
+   */
+  selectElements(selector) {
+    const elements = document.querySelectorAll(selector);
+    
+    if (elements.length === 0) {
+      console.error("Nenhum elemento encontrado para o seletor:", selector);
     }
 
-    /**
-     * Seleciona elementos no DOM com base no seletor fornecido.
-     * @param {string} selector - O seletor para selecionar elementos no DOM.
-     * @returns {NodeList} - Lista de elementos selecionados.
-     */
-    selectElements(selector) {
-        const elements = document.querySelectorAll(selector);
-        if (elements.length === 0) {
-            console.error('Nenhum elemento encontrado para o seletor:', selector);
-        }
-        return elements;
-    }
+    return elements;
+  }
 
-    /**
-     * Itera sobre todos os elementos selecionados e executa uma função de retorno para cada um.
-     * @param {Function} callback - Função de retorno a ser executada para cada elemento.
-     */
-    forEachElement(callback) {
-        this.elements.forEach(callback);
-    }
+  /**
+   * Itera sobre todos os elementos selecionados e executa uma função de retorno para cada um.
+   * @param {Function} callback - Função de retorno a ser executada para cada elemento.
+   */
+  forEachElement(callback) {
+    this.elements.forEach(callback);
+  }
 
-    /**
-     * Adiciona conteúdo HTML ao final do conteúdo de cada elemento selecionado.
-     * @param {string} content - O conteúdo HTML a ser adicionado.
-     * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
-     */
-    append(content) {
-        this.forEachElement(element => {
-            element.innerHTML += content;
-        });
-        return this;
-    }
+  /**
+   * Adiciona conteúdo HTML ao final do conteúdo de cada elemento selecionado.
+   * @param {string} content - O conteúdo HTML a ser adicionado.
+   * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
+   */
+  append(content) {
 
-    /**
-     * Adiciona uma classe CSS a todos os elementos selecionados.
-     * @param {string} className - O nome da classe a ser adicionada.
-     * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
-     */
-    addClass(className) {
-        this.forEachElement(element => {
-            element.classList.add(className);
-        });
-        return this;
-    }
+    this.forEachElement((element) => {
+      element.innerHTML += content;
+    });
 
-    /**
-     * Adiciona um manipulador de eventos a todos os elementos selecionados.
-     * @param {string} eventType - O tipo de evento a ser tratado, como 'click' ou 'mouseover'.
-     * @param {Function} handler - O manipulador de eventos a ser executado quando o evento ocorrer.
-     * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
-     */
-    on(eventType, handler) {
-        this.forEachElement(element => {
-            element.addEventListener(eventType, handler);
-        });
-        return this;
-    }
+    return this;
+  }
 
-    /**
-     * Remove todos os elementos selecionados do DOM.
-     * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
-     */
-    remove() {
-        this.forEachElement(element => {
-            element.remove();
-        });
-        return this;
-    }
+  /**
+   * Adiciona uma classe CSS a todos os elementos selecionados.
+   * @param {string} className - O nome da classe a ser adicionada.
+   * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
+   */
+  addClass(className) {
+
+    this.forEachElement((element) => {
+      element.classList.add(className);
+    });
+
+    return this;
+  }
+
+  /**
+   * Adiciona um manipulador de eventos a todos os elementos selecionados.
+   * @param {string} eventType - O tipo de evento a ser tratado, como 'click' ou 'mouseover'.
+   * @param {Function} handler - O manipulador de eventos a ser executado quando o evento ocorrer.
+   * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
+   */
+  on(eventType, handler) {
+
+    this.forEachElement((element) => {
+      element.addEventListener(eventType, handler);
+    });
+
+    return this;
+  }
+
+  /**
+   * Remove todos os elementos selecionados do DOM.
+   * @returns {JQuery} - A própria instância de JQuery para encadeamento de métodos.
+   */
+  remove() {
+
+    this.forEachElement((element) => {
+      element.remove();
+    });
+
+    return this;
+  }
 }
 
 /**
  * Classe utilitária para fazer requisições AJAX.
  */
 class Ajax {
-    /**
-     * Realiza uma requisição AJAX.
-     * @param {Object} options - Opções para a requisição AJAX, como URL, método, cabeçalhos e dados.
-     * @returns {Promise} - Uma promessa que resolve com os dados da resposta da requisição.
-     */
-    static async ajax(options) {
-        const response = await fetch(options.url, {
-            method: options.method || 'GET',
-            headers: options.headers || { 'Content-Type': 'application/json' },
-            body: options.data || null
-        });
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return await response.json();
+  /**
+   * Realiza uma requisição AJAX.
+   * @param {Object} options - Opções para a requisição AJAX, como URL, método, cabeçalhos e dados.
+   * @returns {Promise} - Uma promessa que resolve com os dados da resposta da requisição.
+   */
+  static async ajax(options) {
+    const response = await fetch(options.url, {
+      method: options.method || "GET",
+      headers: options.headers || { "Content-Type": "application/json" },
+      body: options.data || null,
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status);
     }
+
+    return await response.json();
+  }
 }
 
-// Cria uma instância de JQuery e define o protótipo como a classe Ajax
 const jQueryInstance = (selector) => new JQuery(selector);
 Object.setPrototypeOf(jQueryInstance, Ajax);
 
-// Exporta a instância de JQuery como $
 export const $ = jQueryInstance;
